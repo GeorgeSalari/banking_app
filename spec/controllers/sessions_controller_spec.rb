5f1e2669de711aaebcf5a3179aa947789c72c62d
George Salari <george@gowabi.com>
2021-06-14 01:36:50 +0700

    added test

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe SessionsController, type: :controller do
+  describe 'POST #create' do
+
+    it 'redirects to new action when data not valid' do
+      post :create, params: { user: { email: '', password: '' } }
+      expect(session[:user_id]).to be(nil)
+      expect(response).to redirect_to(action: :new)
+    end
+
+    it 'sign in an account with valid data' do
+      user = User.create(email: 'test@gmail.com', password: 'test', password_confirmation: 'test')
+      params = {
+        email: user.email,
+        password: user.password
+      }
+      post :create, params: { user: params }
+      expect(session[:user_id]).to be(user.id)
+    end
+  end
+
+  describe 'POST #destroy' do
+    it 'allows user to log out' do
+      user = User.create(email: 'test@gmail.com', password: 'test', password_confirmation: 'test')
+      params = {
+        email: user.email,
+        password: user.password
+      }
+      post :create, params: { user: params }
+      expect(session[:user_id]).to be(user.id)
+
+      delete :destroy
+      expect(session[:user_id]).to be(nil)
+      expect(response).to redirect_to(action: :new)
+    end
+  end
+end
