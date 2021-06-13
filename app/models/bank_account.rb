307193f293e1dbf1b520d2915b585f969026aae8
George Salari <george@gowabi.com>
2021-06-14 00:35:44 +0700

    add validations to model

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -1,2 +1,6 @@
 class BankAccount < ApplicationRecord
+  validates :account, :user_id, :balance, presence: true
+  validates :account, uniqueness: true
+  validates :balance, numericality: { greater_than_or_equal_to: 0 }
+  belongs_to :user
 end
--- spec/models/bank_account_spec.rb
+++ spec/models/bank_account_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe BankAccount, type: :model do
 
   ## Test of validations
   it { should validate_presence_of :account }
-  it { should validate_uniqueness_of :account }
   it { should validate_presence_of :user_id }
   it { should validate_presence_of :balance }
 
@@ -18,13 +17,13 @@ RSpec.describe BankAccount, type: :model do
     bank_account = BankAccount.create(user_id: user.id, account: '1234-1234-1234-1234')
     user_1 = User.create(email: 'test1@gmail.com', password: 'test', password_confirmation: 'test')
     bank_account_1 = BankAccount.create(user_id: user_1.id, account: '1234-1234-1234-1234')
-    expect(bank_account.errors.messages).to include('Bank account already exist')
+    expect(bank_account_1.errors.messages[:account]).to include('has already been taken')
   end
 
   it 'validate not have a negative balance' do
     user = User.create(email: 'test@gmail.com', password: 'test', password_confirmation: 'test')
     bank_account = BankAccount.create(user_id: user.id, account: '1234-1234-1234-1234')
     bank_account.update(balance: -5)
-    expect(bank_account.errors.messages).to include("Balance can't be less then 0")
+    expect(bank_account.errors.messages[:balance]).to include("must be greater than or equal to 0")
   end
 end
