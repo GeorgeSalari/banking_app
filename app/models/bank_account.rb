78adda0fca01f004f2baa65446c6c329492ae776
George Salari <george@gowabi.com>
2021-06-14 03:33:59 +0700

    addeed associations for bank accounts

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -2,5 +2,8 @@ class BankAccount < ApplicationRecord
   validates :account, :user_id, :balance, presence: true
   validates :account, uniqueness: true
   validates :balance, numericality: { greater_than_or_equal_to: 0 }
+
   belongs_to :user
+  has_many :from_bank_account, foreign_key: :from_bank_account_id, class_name: "Transaction"
+  has_many :to_bank_account, foreign_key: :to_bank_account_id, class_name: "Transaction"
 end
