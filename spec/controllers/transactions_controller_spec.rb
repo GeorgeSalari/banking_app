85cf8c95c96d19fb15ad3c75d89098f2ae80dd29
George Salari <george@gowabi.com>
2021-06-14 05:36:58 +0700

    added controller and test, added view

--- spec/controllers/transactions_controller_spec.rb
+++ spec/controllers/transactions_controller_spec.rb
@@ -6,29 +6,33 @@ RSpec.describe TransactionsController, type: :controller do
   describe 'Get #new' do
     it 'redirects to login if no user' do
       get :new
-      expect(response).to redirect_to('/transactions/new')
+      expect(response).to redirect_to('/')
     end
   end
 
   describe 'POST #create' do
 
     it 'redirects to new action if no amount' do
+      user_from = User.create(email: 'test@gmail.com', password: 'test', password_confirmation: 'test')
+      session[:user_id] = user_from.id
       post :create, params: {
         transaction: {
           amount: '',
           from_bank_account_id: '',
-          to_bank_account_id: ''
+          to_bank_account: ''
         }
       }
       expect(response).to redirect_to('/transactions/new')
     end
 
     it 'redirects to new action if no from_bank_account_id' do
+      user_from = User.create(email: 'test@gmail.com', password: 'test', password_confirmation: 'test')
+      session[:user_id] = user_from.id
       post :create, params: {
         transaction: {
           amount: 10,
           from_bank_account_id: '',
-          to_bank_account_id: ''
+          to_bank_account: ''
         }
       }
       expect(response).to redirect_to('/transactions/new')
@@ -36,12 +40,13 @@ RSpec.describe TransactionsController, type: :controller do
 
     it 'redirects to new action if no to_bank_account_id' do
       user_from = User.create(email: 'test@gmail.com', password: 'test', password_confirmation: 'test')
+      session[:user_id] = user_from.id
       from_bank_account = user_from.bank_accounts.first
       post :create, params: {
         transaction: {
           amount: 10,
           from_bank_account_id: from_bank_account.id,
-          to_bank_account_id: ''
+          to_bank_account: ''
         }
       }
       expect(response).to redirect_to('/transactions/new')
@@ -49,6 +54,7 @@ RSpec.describe TransactionsController, type: :controller do
 
     it 'redirects to new action if from user have no balance' do
       user_from = User.create(email: 'test@gmail.com', password: 'test', password_confirmation: 'test')
+      session[:user_id] = user_from.id
       from_bank_account = user_from.bank_accounts.first
       to_user = User.create(email: 'test1@gmail.com', password: 'test', password_confirmation: 'test')
       to_bank_account = to_user.bank_accounts.first
@@ -56,7 +62,7 @@ RSpec.describe TransactionsController, type: :controller do
         transaction: {
           amount: 10,
           from_bank_account_id: from_bank_account.id,
-          to_bank_account_id: to_bank_account.id,
+          to_bank_account: to_bank_account.account,
         }
       }
       expect(response).to redirect_to('/transactions/new')
@@ -64,15 +70,16 @@ RSpec.describe TransactionsController, type: :controller do
 
     it 'redirects to new action if from user have less balance then transfer amount' do
       user_from = User.create(email: 'test@gmail.com', password: 'test', password_confirmation: 'test')
+      session[:user_id] = user_from.id
       from_bank_account = user_from.bank_accounts.first
-      from_bank_account.update(balance: 15)
+      from_bank_account.update(balance: 5)
       to_user = User.create(email: 'test1@gmail.com', password: 'test', password_confirmation: 'test')
       to_bank_account = to_user.bank_accounts.first
       post :create, params: {
         transaction: {
           amount: 10,
           from_bank_account_id: from_bank_account.id,
-          to_bank_account_id: to_bank_account.id,
+          to_bank_account: to_bank_account.account,
         }
       }
       expect(response).to redirect_to('/transactions/new')
@@ -80,6 +87,7 @@ RSpec.describe TransactionsController, type: :controller do
 
     it 'create transaction with valid data and change users balance' do
       user_from = User.create(email: 'test@gmail.com', password: 'test', password_confirmation: 'test')
+      session[:user_id] = user_from.id
       from_bank_account = user_from.bank_accounts.first
       from_bank_account.update(balance: 15)
       to_user = User.create(email: 'test1@gmail.com', password: 'test', password_confirmation: 'test')
@@ -88,12 +96,12 @@ RSpec.describe TransactionsController, type: :controller do
         transaction: {
           amount: 10,
           from_bank_account_id: from_bank_account.id,
-          to_bank_account_id: to_bank_account.id,
+          to_bank_account: to_bank_account.account,
         }
       }
       expect(Transaction.count).to be(1)
-      expect(from_bank_account.balance).to be(5)
-      expect(to_bank_account_id.balance).to be(10)
+      expect(user_from.bank_accounts.first.balance).to be(5.to_f)
+      expect(to_user.bank_accounts.first.balance).to be(10.to_f)
     end
   end
 end
